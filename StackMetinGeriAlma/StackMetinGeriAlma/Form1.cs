60a7b0a6b31c2dce77246047f02193563f650371
Mehmet İslam Dağ <dagislam4734@gmail.com>
2025-05-29 23:21:12 +0300

    Add files via upload

---
+++ StackMetinGeriAlma/StackMetinGeriAlma/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StackMetinGeriAlma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Stack<string> geriAl = new Stack<string>();
+        Stack<string> ileriAl = new Stack<string>();
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (!geriAl.Contains(textBox1.Text));
+            {
+                ileriAl.Clear();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ileriAl.Count > 0)
+            {
+                string ileriMetin = ileriAl.Pop();
+                geriAl.Push(ileriMetin);
+                textBox1.Text = ileriMetin;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (geriAl.Count > 1)
+            {
+                string hedef = geriAl.Pop();
+                ileriAl.Push(hedef);
+                textBox1.Text = geriAl.Peek();
+
+
+            }
+        }
+    }
+}
---
+++ queueUygulaması/queueUygulaması/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace queueUygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Queue kuyruk = new Queue();
+        int sira = 0;
+        private void listele()
+        {
+            listBox1.Items.Clear();
+            foreach (var sira in kuyruk)
+            {
+                listBox1.Items.Add(sira);
+            }
+                }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sira++;
+            kuyruk.Enqueue(sira);
+            listele();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            kuyruk.Dequeue();
+            listele();
+        }
+    }
+}
