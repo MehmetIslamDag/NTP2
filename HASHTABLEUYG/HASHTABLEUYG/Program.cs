2fa1f8c2e6ae5bd2b975db3dc5f6ce9802e86b8a
Mehmet İslam Dağ <dagislam4734@gmail.com>
2025-05-29 23:27:05 +0300

    Add files via upload

---
+++ HASHTABLEUYG/HASHTABLEUYG/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HASHTABLEUYG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ogrenciler = new Hashtable();
+            ogrenciler.Add(43, "Damla");
+            ogrenciler.Add(286, "Mehmet İslam");
+            ogrenciler.Add(147, "Baki");
+            ogrenciler.Add("yusuf", 72);
+
+
+            ogrenciler.Remove(43);
+            ogrenciler.Remove("burak");
+
+
+            ogrenciler[147] = "emirhan";
+            ogrenciler["ipek"] = 205;
+
+
+            foreach (var anahtar in ogrenciler.Values)
+            {
+                Console.WriteLine(anahtar);
+
+            }
+            foreach (DictionaryEntry eleman in ogrenciler)
+            {
+                Console.WriteLine(eleman.Key + "-" + eleman.Value);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ SortedListKoleksiyonu/SortedListKoleksiyonu/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedListKoleksiyonu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList ogrenciler = new SortedList();
+
+            ogrenciler.Add(1932, "Arda");
+            ogrenciler.Add(72, "yusuf");
+            ogrenciler.Add(308, "Berkay");
+
+            ogrenciler.Remove(308);
+            ogrenciler[72] = "Eyüp";
+            foreach (DictionaryEntry eleman in ogrenciler)
+            {
+                Console.WriteLine(eleman.Key + "-" + eleman.Value);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
